17912e2db39b9a035e14c162e03474e1d0359d16
van-anan <109599545+van-anan@users.noreply.github.com>
2022-07-19 16:59:37 +0400

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,118 +1,91 @@
 #include <iostream>
+
 #include <exception>
-template <typename T>
-class Stack {
-public:
-	Stack();
-	Stack(const Stack<T>&);
-	Stack& operator=(const Stack<T>&);
-	~Stack();
 
-	bool is_empty() const { return  m_top == -1; }
-	int size() const { return (m_top + 1); }
-	T& top();
-	const T& top() const;
-	void push(T);
-	void pop();
+template < typename T >
+  class Stack {
+    public:
+      Stack();
+    Stack(const Stack < T > & );
+    Stack < T > & operator = (const Stack < T > & );
+    ~Stack();
+    T & top() const;
+    bool is_empty() const;
+    int size() const;
+    void push(T);
+    void pop();
+    private:
+      T * m_data;
+    int m_top;
+    int m_max_size;
+    static
+    const int max_size = 100;
+  };
+template < typename T >
+  Stack < T > ::Stack(): m_data(nullptr), m_top(-1), m_max_size(max_size) {
+    m_data = new int[m_max_size];
+  }
+template < typename T >
+  Stack < T > ::Stack(const Stack & other) {
+    m_max_size = other.m_max_size;
+    m_top = other.m_top;
+    m_data = new T[m_max_size];
+    for (int i = 0; i < m_max_size; i++) {
+      m_data[i] = other.m_data[i];
+    }
 
-private:
-	static const int MAX_SIZE = 100;
+  }
+template < typename T >
+  Stack < T > & Stack < T > ::operator = (const Stack < T > & other) {
+    delete[] m_data;
+    m_max_size = other.m_max_size;
+    m_top = other.m_top;
+    for (int i = 0; i < m_max_size; i++) {
+      m_data[i] = other.m_data[i];
+    }
+    return *this;
+  }
+template < typename T >
+  Stack < T > ::~Stack() {
+    delete[] m_data;
+  }
+template < typename T >
+  T & Stack < T > ::top() const {
+    if (!is_empty()) {
+      return m_data[m_top];
+    }
+	else{
+	    throw std::exception("The array is empty");
+	  }
+  }
+template < typename T >
+  bool Stack < T > ::is_empty() const {
+    return m_top == -1;
+  }
+template < typename T >
+  int Stack < T > ::size() const {
+    return m_top + 1;
+  }
+template < typename T >
+  void Stack < T > ::push(T element) {
+    if (m_top + 1 == m_max_size) {
+      m_max_size *= 2;
+      int * tmp = new int[m_max_size];
+      for (int i = 0; i < m_top; i++) {
+        tmp[i] = m_data[i];
+      }
+      delete[] m_data;
+      m_data = tmp;
+    }
+    m_top++;
+    m_data[m_top] = element;
+  }
+template < typename T >
+  void Stack < T > ::pop() {
+    if (!is_empty()) {
+      --m_top;
+    } else {
+      throw std::exception("The  stack is empty");
+    }
 
-	int m_max_size;
-	int m_top;
-	T* m_data;
-};
-
-template <typename T>
-Stack<T>::Stack() : m_max_size(MAX_SIZE), m_top(-1), m_data(nullptr) {
-	m_data = new T[m_max_size]{};
-}
-
-template <typename T>
-Stack<T>::Stack(const Stack<T>& object) : m_max_size(object.m_max_size), m_top(object.m_top), m_data(nullptr) {
-	m_data = new T[m_max_size]{};
-	for (int i = 0; i <= m_top; ++i) {
-		m_data[i] = object.m_data[i];
-	}
-}
-
-template <typename T>
-Stack<T>& Stack<T>::operator=(const Stack<T>& object) {
-	if (this != &object) {
-		delete[] m_data;
-		m_top = object.m_top;
-		m_max_size = object.m_max_size;
-		m_data = new T[m_max_size]{};
-		for (int i = 0; i <= m_top; ++i) {
-			m_data[i] = object.m_data[i];
-		}
-	}
-	return *this;
-}
-
-template <typename T>
-Stack<T>::~Stack() {
-	delete[] m_data;
-}
-
-template <typename T>
-T& Stack<T>::top() {
-	if (!is_empty()) {
-		return m_data[m_top];
-	}
-	throw std::exception("The stack is empty.");
-}
-
-template <typename T>
-const T& Stack<T>::top() const {
-	if (!is_empty()) {
-		return m_data[m_top];
-	}
-	throw std::exception("The stack is empty.");
-}
-
-template <typename T>
-void Stack<T>::push(T element) {
-	if (m_top + 1 == m_max_size) {
-		m_max_size *= 2;
-		T* tmp = new T[m_max_size]{};
-		for (int i = 0; i <= m_top; ++i) {
-			tmp[i] = m_data[i];
-		}
-		delete[] m_data;
-		m_data = tmp;
-	}
-	++m_top;
-	m_data[m_top] = element;
-}
-
-template <typename T>
-void Stack<T>::pop() {
-	if (!is_empty()) {
-		--m_top;
-	}
-	else {
-		throw std::exception("The stack is empty.");
-
-	}
-}
-int main() {
-	Stack<int> a;
-	a.push(5);
-	a.push(6);
-	a.push(7);
-	a.push(8);
-	Stack<int> b(a);
-	b.push(9);
-	std::cout << b.top();
-	Stack<int> c;
-	try {
-		c.pop();
-	}
-	catch (const std::exception&e) {
-		std::cout <<e.what();
-	}
-
-	std::cout << b.top();
-	return 0;
-}
+  }
